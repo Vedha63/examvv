ead6e6076e1fb0b8db04d2da1bfc843c537a40d1
vedha ponnam <245122749040@mvsrec.edu.in>
2025-07-25 12:25:26 +0530

    Add files via upload

---
+++ Arith.java
@@ -0,0 +1,23 @@
+// Arith.java
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        // Perform arithmetic operations
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        // Print results
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
